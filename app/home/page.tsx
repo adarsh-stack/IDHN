e7901bd36975f5dad38e8b5522e63845ee33ea50
adarsh-stack <adarshagarwal354@gmail.com>
2026-06-30 13:52:19 +0530

    Major updates in appliation

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,19 +1,14 @@
-
 "use client";
 
 import React from "react";
 import Header from "../Components/Header";
-// 1. Notice the capital "C" in Components to match your folder
-import ProtectedRoute from "@/app/Components/ProtectedRoute"; 
 import ClinicalDashboardView from "@/app/Components/ClinicalDashboardView";
 
 export default function HomePage() {
   return (
-    // 2. Lock this route so ONLY Doctors can render it
-    <ProtectedRoute allowedRoles={['Doctor']}>
+    <>
       <Header />
       <ClinicalDashboardView />
-      
-    </ProtectedRoute>
+    </>
   );
 }
